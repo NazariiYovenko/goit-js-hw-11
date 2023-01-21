2ee527eacfc865fefd0cd8cd38d8f888cd392b78
NazariiYovenko <nazarjovenko@gmail.com>
2023-01-21 15:27:29 +0200

    re-add project

---
+++ src/fetchPhotos.js
@@ -0,0 +1,51 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import axios from 'axios';
+
+export default class PixabayImg {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  async getImage() {
+    const searchParams = new URLSearchParams({
+      key: '33013625-015f9d261f712aba654b673d5',
+      q: `${this.searchQuery}`,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: 'true',
+      per_page: 40,
+      page: this.page,
+    });
+    const url = `https://pixabay.com/api/?${searchParams}`;
+
+    try {
+      const response = await axios.get(url);
+
+      if (response.data.hits.length === 0) {
+        return Notify.warning(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      }
+      if (this.page === 1) {
+        Notify.success(`Hooray! We found ${response.data.totalHits} images.`);
+      }
+
+      this.page += 1;
+      return response.data;
+    } catch (error) {
+      Notify.failure(error.message);
+    }
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,98 @@
+import './css/styles.css';
+import PixabayImg from './fetchPhotos';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import throttle from 'lodash.throttle';
+
+const refs = {
+  form: document.querySelector('#search-form'),
+  galleryBox: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+
+const maxPage = 13;
+
+const pixabayImg = new PixabayImg();
+refs.loadMoreBtn.hidden = true;
+let totalHitsForPage = 0;
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  const form = e.currentTarget;
+  pixabayImg.query = form.elements.searchQuery.value;
+  pixabayImg.resetPage();
+  pixabayImg.getImage().then(markupImgSearch);
+  resetMarkup();
+}
+
+function markupImgSearch(data) {
+  const markup = data.hits
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<div class="photo-card"><a href="${largeImageURL}">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" class="image" /></a>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b> ${likes}
+    </p>
+    <p class="info-item">
+      <b>Views</b> ${views}
+    </p>
+    <p class="info-item">
+      <b>Comments</b> ${comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads</b> ${downloads}
+    </p>
+  </div>
+</div>`;
+      }
+    )
+    .join('');
+
+  refs.galleryBox.insertAdjacentHTML('beforeend', markup);
+
+  totalHitsCount(data);
+
+  new SimpleLightbox('.photo-card a', { captionDelay: 250 }).refresh();
+}
+
+function resetMarkup() {
+  refs.galleryBox.innerHTML = '';
+}
+
+function totalHitsCount(data) {
+  totalHitsForPage += data.hits.length;
+  if (totalHitsForPage >= data.totalHits) {
+    Notify.info("We're sorry, but you've reached the end of search results.");
+  }
+}
+
+/////// infinity scroll ////////
+window.addEventListener('scroll', throttle(onScroll, 500));
+
+function onScroll() {
+  if (pixabayImg.page > maxPage) {
+    return;
+  }
+
+  const { scrollHeight, scrollTop, clientHeight } = document.documentElement;
+
+  const scrollPosition = scrollHeight - clientHeight;
+  const scrollTopRound = Math.round(scrollTop);
+
+  if (scrollTopRound >= scrollPosition - 1) {
+    pixabayImg.getImage().then(markupImgSearch);
+  }
+}
